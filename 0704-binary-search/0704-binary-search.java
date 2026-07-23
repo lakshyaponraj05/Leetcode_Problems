fa718d792e4ffe2e18abce0c0ee1208d04cbfac1
Lakshya.P <plak.csbs2024@rmd.ac.in>
2026-07-23 12:48:25 +0530

    Time: 0 ms (100%), Space: 48.6 MB (13.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,7 +3,7 @@ class Solution {
         int l=0;
         int r=nums.length-1;
         while(l<=r){
-             int m=l+(r-l)/2;
+             int m=(l+r)/2;
              if(nums[m]==target){
                 return m;
              }
