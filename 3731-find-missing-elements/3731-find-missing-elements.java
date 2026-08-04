2308dfedfc4a4febfa02f8600fdb48ea3c5b1f53
Lakshya.P <plak.csbs2024@rmd.ac.in>
2026-08-04 10:27:08 +0530

    Time: 6 ms (39.24%), Space: 47.5 MB (5.33%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -5,7 +5,10 @@ class Solution {
         int cur = nums[0];
 
         for (int x : nums) {
-            while (cur < x) res.add(cur++);
+            while (cur < x){
+                res.add(cur);
+                cur++;
+            }
             cur = x + 1;
         }
         return res;
